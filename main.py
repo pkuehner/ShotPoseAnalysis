e21ca43a411d5c8b19187ae3b012ee6d599b9bb0
Pascal Kühner <pascal.kuehner@rwth-aachen.de>
2023-09-14 14:58:13 +0200

    Adjusting stuff

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import cv2
 import sys
 from processor import FrameProcessor
 
-from util import get_landmarks_list
-
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose 
 pose = mp_pose.Pose(min_detection_confidence=0.2, min_tracking_confidence=0.2) 
-print(pose) 
 sys.argv.append("IMG_4290.MOV")
 cap = cv2.VideoCapture(sys.argv[1])
 if cap.isOpened() == False:
@@ -20,23 +17,19 @@ outdir, inputflnm = sys.argv[1][:sys.argv[1].rfind('/')+1], sys.argv[1][sys.argv
 inflnm, inflext = inputflnm.split('.')
 out_filename = f'{outdir}{inflnm}_annotated.{inflext}'
 out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-frameProcessor = FrameProcessor()
+frameProcessor = FrameProcessor(pose)
 while cap.isOpened():
     ret, image = cap.read()
     if not ret:
         break
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)         
     image.flags.writeable = False
-    results = pose.process(image)
     try:
-        landmarks_list = get_landmarks_list(image, results)
+        frameProcessor.processImage(image)
     except:
-        print("Can't process frame, no points")
         continue
-    frameProcessor.processPose(image, landmarks_list)
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)      
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     out.write(image)
 
 pose.close()
--- processor.py
+++ processor.py
@@ -1,18 +1,35 @@
 from typing import List
 from draw_util import draw_angle
 from dtypes import Frame, Landmarker
-from util import get_elbow_right_angle, get_knee_right_angle
-
+from util import get_elbow_right_angle, get_hip_right_angle, get_knee_right_angle, get_landmarks_list, get_shoulder_right_angle, get_wrist_right_angle
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
 
 class FrameProcessor:
     frames: List[Frame] = []
+    pose: mp.solutions.pose.Pose
 
-    def processPose(self, image, landmarks: List[Landmarker]):
+    def __init__(self, pose: mp.solutions.pose.Pose):
+        self.pose = pose
+
+    def processImage(self, image):
+        results = self.pose.process(image)
+        try:
+            landmarks_list = get_landmarks_list(image, results)
+        except Exception as e:
+            print("Can't process frame, no points")
+            raise e
+        
         angles = [
-            get_elbow_right_angle(landmarks),
-            get_knee_right_angle(landmarks)
+            get_elbow_right_angle(landmarks_list),
+            get_knee_right_angle(landmarks_list),
+            get_wrist_right_angle(landmarks_list),
+            get_hip_right_angle(landmarks_list),
+            get_shoulder_right_angle(landmarks_list),
         ]
         for angle in angles:
             draw_angle(image, angle)
-            self.frames.append(Frame(image, landmarks, angles))
+            self.frames.append(Frame(image, landmarks_list, angles))
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
         
